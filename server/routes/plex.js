1b7030d4313a807af83b661e13c1f5f4c752503c
Tom Matheussen <tommatheussen5@hotmail.com>
2017-04-16 18:31:55 +0200

    Fixed empty response from plex search api

--- server/routes/plex.js
+++ server/routes/plex.js
@@ -8,7 +8,7 @@ var plexClient = new plexAPI(require('./../config'));
 router.get('/', (req, res) => {
   let search = req.query.search;
   plexClient.query(`/search?query=${search}`).then(function (response) {
-    res.json(response.MediaContainer.Metadata);
+    res.json(response.MediaContainer.Metadata || []);
   }, function (err) {
     console.log(`Error searching: ${err}`);
     res.send(err);
