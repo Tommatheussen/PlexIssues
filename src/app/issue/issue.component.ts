a05e0645c5832680e1baee67eaa8fdd6d96a5c39
Tom Matheussen <tommatheussen5@hotmail.com>
2017-04-20 23:43:32 +0200

    started visualising metadata on issue cards

--- src/app/issue/issue.component.ts
+++ src/app/issue/issue.component.ts
@@ -1,18 +1,48 @@
-import { Component,  Input } from '@angular/core';
+import { Component,  Input, OnInit } from '@angular/core';
 
 import { Issue } from '../issue';
+import { PlexItem } from '../plex-item';
+
 import { IssueService } from '../issue.service';
+import { PlexService } from '../plex.service';
 
 @Component({
   selector: 'plexissues-issue',
   templateUrl: './issue.component.html',
   styleUrls: ['./issue.component.css'],
-  providers: [IssueService]
+  providers: [
+    IssueService,
+    PlexService
+  ]
 })
-export class IssueComponent {
+export class IssueComponent implements OnInit{
   @Input() issue: Issue;
 
-  constructor(private issueService: IssueService) { }
+  public metadata: PlexItem;
+
+  dict: { [index: string]: string; } = {
+    movie: 'local_movies',
+    show: 'tv',
+    episode: 'tv',
+    artist: 'person',
+    album: 'album',
+    track: 'music_note'
+  }
+
+  constructor(
+    private issueService: IssueService,
+    private plexService: PlexService) { }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.plexService.getPlexItem(this.issue.item)
+      .subscribe((item) => {
+        console.log(item);
+        this.metadata = item;
+        //TODO: Add content to card
+      });
+  }
 
   updateIssue(status): void {
     const copy = { ...this.issue };
