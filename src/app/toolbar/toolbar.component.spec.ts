dcaf62eebffa1da6a6bb0d270aab60f507f2d965
Tom Matheussen <tmatheussen@avia-gis.com>
2017-04-21 16:28:29 +0200

    More tests

--- src/app/toolbar/toolbar.component.spec.ts
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -1,10 +1,16 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { MaterialModule } from '@angular/material';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { ToolbarComponent } from './toolbar.component';
 
+describe('ToolbarComponent', () => {
+  let comp: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
-describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -16,9 +22,20 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the toolbar', async(() => {
-    const fixture = TestBed.createComponent(ToolbarComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolbarComponent);
+    comp = fixture.componentInstance;
+
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+  })
+
+  it('should create the toolbar', () => {
+    expect(comp).toBeTruthy();
+  });
+
+
+  it('should have a header', () => {
+    expect(el.textContent).toEqual('Test');
+  });
 });
